072a632f48716d4a902b86676409401f9acd43ff
RikaTob <rikatob@hotmail.com>
2023-04-21 22:13:39 +0200

    -Added tft screen basics -print state to screen -> WORKING

--- Game/Game.ino
+++ Game/Game.ino
@@ -1,7 +1,36 @@
 // EKSAMEN EMBEDDED SYSTEMS
+
+/*
+<<<<<<<<SPI>>>>>>>>>
+CS    -> 10 ( CHIP SELECT TFT)
+CS    -> 6 (CHIP SELECT RFID) 
+MOSI  -> 11 ( MASTER IN SLAVE OUT)
+MISO  -> 12 ( MASTER IN SLAVE OUT)
+SCK   -> 13 ( CLOCK SPEED)
+*/
+
+/////////////////////////////////////////////////////Libraries/////////////////////////////////////////////////////
+/*
+<<<<<<<<<<<<<<<<<<<< ST7789 >>>>>>>>>>>>>>>>>>>>
+*/
+#include <Adafruit_GFX.h>
+#include <Adafruit_ST7789.h>
+#include <SPI.h>
+
+//////////////////////////////////////////////////////Defines//////////////////////////////////////////////////////
 #define WIRE_PIN 2
 #define BUZZER_PIN 3
 #define GAME_BUTTON_PIN 4
+// TFT
+#define TFT_CS 10
+#define TFT_RST 9
+#define TFT_DC 8
+#define DEFAULT_TEXT_SIZE 3
+
+///////////////////////////////////////////////////Objects/Inits///////////////////////////////////////////////////
+Adafruit_ST7789 tft = Adafruit_ST7789(TFT_CS, TFT_DC, TFT_RST);
+
+/////////////////////////////////////////////////////Variables/////////////////////////////////////////////////////
 /*
         STATE MACHINE
 IDLE 
@@ -22,8 +51,6 @@ enum State {
   GAME_OVER
 };
 
-
-// VARIABLES
 enum State currentState;
 // Set it to true so the first time idle runs the "setup" for idle state.
 bool stateChanged = true;
@@ -32,19 +59,23 @@ bool stateChanged = true;
 unsigned long debounceDuration = 150;
 unsigned long lastTimeButtonWasPressed = 0;
 
-void setup() {
-  // state should start as idle.
-  currentState = IDLE;
 
+
+void setup() {
+  // State should start as idle.
+  currentState = IDLE;
   Serial.begin(9600);
+
+  //<BASE>
   // Use pullup to get less components on the board,
   // estimated 20 - 50 K resistor in arduino,
   // if i want to have more control later on i can use pulldown and and add the chosen resistor.
   pinMode(WIRE_PIN, INPUT_PULLUP);
   pinMode(BUZZER_PIN, OUTPUT);
   pinMode(GAME_BUTTON_PIN, INPUT_PULLUP);
-
   digitalWrite(BUZZER_PIN, LOW);
+  //</BASE>
+  TftInitiate();
 }
 
 void loop() {
@@ -64,10 +95,10 @@ void loop() {
 
 void Idle() {
   if (stateChanged) {
-    Serial.println("GAME IN IDLE MODE");
+    DrawText("GAME IN IDLE MODE!", ST77XX_RED, DEFAULT_TEXT_SIZE, 15, 40, true);
+    //Serial.println("GAME IN IDLE MODE");
     stateChanged = false;
   }
-
   byte buttonPressed = CheckButton(GAME_BUTTON_PIN);
 
   // Game button pressed, then start game.
@@ -83,7 +114,8 @@ void Idle() {
 */
 void Game() {
   if (stateChanged) {
-    Serial.println("GAME STARTED!");
+    DrawText("GAME STARTED!", ST77XX_RED, DEFAULT_TEXT_SIZE, 5, 55, true);
+    //Serial.println("GAME STARTED!");
     stateChanged = false;
   }
   int wireState = digitalRead(WIRE_PIN);
@@ -93,8 +125,6 @@ void Game() {
     delay(1000);
     currentState = GAME_OVER;
     stateChanged = true;
-  } else {
-    //digitalWrite(BUZZER_PIN, LOW);
   }
 }
 
@@ -103,7 +133,9 @@ void Game() {
 */
 void GameOver() {
   if (stateChanged) {
-    Serial.println("GAME OVER!");
+    tft.fillScreen(ST77XX_BLACK);
+    DrawText("GAME OVER!", ST77XX_RED, DEFAULT_TEXT_SIZE, 25, 55, true);
+    //Serial.println("GAME OVER!");
     digitalWrite(BUZZER_PIN, LOW);
     stateChanged = false;
   }
@@ -125,4 +157,30 @@ int CheckButton(int buttonPin) {
     }
   }
   return false;
+}
+
+
+void TftInitiate() {
+  // Reset the TFT to get a "clean" start.
+  digitalWrite(TFT_RST, HIGH);
+  digitalWrite(TFT_RST, LOW);
+
+  // Initiate ST7789 1.14" 240x135.
+  tft.init(135, 240);
+  // Set start point to black screen.
+  tft.fillScreen(ST77XX_BLACK);
+  // Rotate screen to show widesreen where 0,0 is top 135 line and left 240 line.
+  tft.setRotation(1);
+}
+
+// Print text to the screen in a certain color('RGB565 16bit' "ST77XX_<color>" pre-defined from lib), size, position(x=COLUMN, y=ROW) and clearing screen.
+void DrawText(char* text, uint16_t color, uint8_t size, uint8_t x, uint8_t y, bool clearScreen) {
+  if (clearScreen) {
+    tft.fillScreen(ST77XX_BLACK);
+  }
+  tft.setCursor(x, y);
+  tft.setTextColor(color);
+  tft.setTextSize(size);
+  tft.setTextWrap(true);
+  tft.print(text);
 }
