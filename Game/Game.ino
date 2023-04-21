90d1987284c5a8359996c0d8ccd20a0b3942e323
RikaTob <rikatob@hotmail.com>
2023-04-21 16:22:20 +0200

    Basic wire setup -> works

--- Game/Game.ino
+++ Game/Game.ino
@@ -1,12 +1,19 @@
 // EKSAMEN EMBEDDED SYSTEMS
 
+#define WIRE_PIN 8
+
 
 void setup() {
-  // put your setup code here, to run once:
-
+  Serial.begin(9600);
+  // Use pullup to get less components on the board,
+  // estimated 20 - 50 K resistor in arduino, 
+  // if i want to have more control later on i can use pulldown and and add the chosen resistor. 
+  pinMode(WIRE_PIN, INPUT_PULLUP);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  int wireState = digitalRead(WIRE_PIN);
+
+  Serial.println(wireState);
 
 }
